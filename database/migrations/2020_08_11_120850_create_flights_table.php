c3048eaa5ec81daed71f243cfdac9f4e88c83e43
AndrewGitHabSource <125652a>
2020-08-11 15:23:59 +0300

    add migrations add seeds

---
+++ database/migrations/2020_08_11_120850_create_flights_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->string('from');
+            $table->string('where');
+            $table->text('description')->nullable();
+            $table->time('time')->nullable();
+            $table->decimal('price');
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->integer('travelers')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -31,12 +32,42 @@ class DatabaseSeeder extends Seeder
         ]]);
 
         DB::table('hotels')->insert([
-            'name' => 'Luxury Hotel in Paris',
-            'address' => '291 South 21th Street, Suite 721 New York NY 10016',
-            'description' => 'When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.',
-            'price' => 40,
-            'rating' => 4,
-            'popular' => true
+            [
+                'id' => 1,
+                'name' => 'Luxury Hotel in Paris',
+                'address' => '291 South 21th Street, Suite 721 New York NY 10016',
+                'description' => 'When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.',
+                'price' => 40,
+                'rating' => 4,
+                'popular' => true
+            ],
+            [
+                'id' => 2,
+                'name' => 'Luxury Hotel in Paris 1',
+                'address' => '291 South 21th Street, Suite 721 New York NY 12',
+                'description' => 'When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.',
+                'price' => 24,
+                'rating' => 4,
+                'popular' => true
+            ],
+            [
+                'id' => 3,
+                'name' => 'Luxury Hotel in Paris 2',
+                'address' => '291 South 21th Street, Suite 721 New York NY 1s',
+                'description' => 'When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.',
+                'price' => 20,
+                'rating' => 4,
+                'popular' => true
+            ],
+            [
+                'id' => 4,
+                'name' => 'Luxury Hotel in Paris 5',
+                'address' => '291 South 21th Street, Suite 721 New York NY 1s',
+                'description' => 'When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.',
+                'price' => 48,
+                'rating' => 5,
+                'popular' => true
+            ]
         ]);
 
         DB::table('ratings')->insert([
@@ -46,22 +77,116 @@ class DatabaseSeeder extends Seeder
         ]);
 
         DB::table('rooms')->insert([
-            'name' => 'Hotel, Italy',
-            'address' => 'Miami, Fl',
-            'description' => 'Far far away, behind the word mountains, far from the countries',
-            'price' => 40,
-            'hotel_id' => 1,
-            'image' => '/images/room-4.jpg'
+            [
+                'name' => 'Hotel, Italy',
+                'address' => 'Miami, Fl',
+                'description' => 'Far far away, behind the word mountains, far from the countries',
+                'price' => 40,
+                'hotel_id' => 1,
+                'image' => '/images/room-4.jpg'
+            ],
+            [
+                'name' => 'Hotel 1 Room',
+                'address' => 'Miami, Fl',
+                'description' => 'Far far away, behind the word mountains, far from the countries',
+                'price' => 40,
+                'hotel_id' => 1,
+                'image' => '/images/room-4.jpg'
+            ],
+            [
+                'name' => 'Hotel, Italy 1',
+                'address' => 'Miami',
+                'description' => 'Far far away, behind the word mountains, far from the countries',
+                'price' => 40,
+                'hotel_id' => 2,
+                'image' => '/images/room-4.jpg'
+            ],
+            [
+                'name' => 'Hotel',
+                'address' => 'Miami 1258',
+                'description' => 'Far far away, behind the word mountains, far from the countries',
+                'price' => 40,
+                'hotel_id' => 3,
+                'image' => '/images/room-4.jpg'
+            ],
+            [
+                'name' => 'Hotel',
+                'address' => 'Miami',
+                'description' => 'Far far away, behind the word mountains, far from the countries',
+                'price' => 40,
+                'hotel_id' => 4,
+                'image' => '/images/room-4.jpg'
+            ]
         ]);
 
         DB::table('galleries')->insert([
             [
                 'image' => '/images/hotel-1.jpg',
                 'hotel_id' => 1
-            ],[
+            ],
+            [
                 'image' => '/images/hotel-2.jpg',
                 'hotel_id' => 1
+            ],
+            [
+                'image' => '/images/hotel-1.jpg',
+                'hotel_id' => 2
+            ],
+            [
+                'image' => '/images/hotel-2.jpg',
+                'hotel_id' => 2
+            ],
+            [
+                'image' => '/images/hotel-1.jpg',
+                'hotel_id' => 3
+            ],
+            [
+                'image' => '/images/hotel-2.jpg',
+                'hotel_id' => 3
+            ],
+            [
+                'image' => '/images/hotel-1.jpg',
+                'hotel_id' => 4
+            ],
+            [
+                'image' => '/images/hotel-2.jpg',
+                'hotel_id' => 4
             ]
+        ]);
+
+        DB::table('flights')->insert([
+            [
+                'from' => 'Kiev',
+                'where' => 'Moscow',
+                'description' => 'description description description description description',
+                'date_start' => Carbon::parse('11-08-2020'),
+                'date_end' => Carbon::parse('12-08-2020'),
+                'price' => 55.5
+            ],
+            [
+                'from' => 'Moscow',
+                'where' => 'Kiev',
+                'description' => 'description description description description description',
+                'date_start' => Carbon::parse('11-08-2020'),
+                'date_end' => Carbon::parse('12-08-2020'),
+                'price' => 55.5
+            ],
+            [
+                'from' => 'Kiev',
+                'where' => 'New City',
+                'description' => 'description description description description description',
+                'date_start' => Carbon::parse('14-08-2020'),
+                'date_end' => Carbon::parse('15-08-2020'),
+                'price' => 55.5
+            ],
+            [
+                'from' => 'Kiev',
+                'where' => 'Smela',
+                'description' => 'description description description description description',
+                'date_start' => Carbon::parse('18-08-2020'),
+                'date_end' => Carbon::parse('18-08-2020'),
+                'price' => 55.5
+            ],
         ]);
     }
 }
